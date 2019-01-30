6ea44419f2c97c2156cfd7a13b09e223c90f75b8
galcyurio <galcyurio@gmail.com>
2019-01-30 18:15:57 +0900

    re #1 step1 으로 업데이트

--- dependency-injection/src/main/kotlin/com/github/galcyurio/Framework.kt
+++ dependency-injection/src/main/kotlin/com/github/galcyurio/Framework.kt
@@ -1,6 +1,11 @@
 package com.github.galcyurio
 
+import arrow.Kind
 import arrow.core.Try
+import arrow.core.left
+import arrow.core.right
+import arrow.effects.typeclasses.Async
+import arrow.typeclasses.ApplicativeError
 import arrow.typeclasses.basics.*
 
 interface NetworkOperations {
@@ -18,4 +23,18 @@ interface DaoOperations {
 
     fun Index.queryCompany(): Try<UserDao> =
         Try { dao.query("SELECT * FROM companies WHERE companyId = $this") }
+}
+
+interface NetworkOperationsSync<F> : ApplicativeError<F, Throwable> {
+    val network: NetworkModule
+
+    fun Index.requestUser(): Kind<F, UserDto> =
+        catch { network.fetch(this, mapOf("1" to "2")) }
+}
+
+interface DaoOperationsSync<F> : ApplicativeError<F, Throwable> {
+    val dao: DaoDatabase
+
+    fun Index.queryUser(): Kind<F, UserDao> =
+        catch { dao.query("SELECT * FROM users WHERE userId = $this") }
 }
