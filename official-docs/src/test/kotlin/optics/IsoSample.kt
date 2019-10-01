09ad8220add4821349bf6aa089cd6f22d86eea85
galcyurio <galcyurio@gmail.com>
2019-10-01 14:09:32 +0900

    IsoSample 일부 추가

--- official-docs/src/test/kotlin/optics/IsoSample.kt
+++ official-docs/src/test/kotlin/optics/IsoSample.kt
@@ -1,12 +1,20 @@
 package optics
 
+import arrow.core.Try
 import arrow.core.Tuple2
+import arrow.core.extensions.`try`.functor.functor
 import arrow.core.toT
 import arrow.optics.Iso
 import org.junit.Test
 
 
 class IsoSample {
+    data class Point2D(val x: Int, val y: Int)
+
+    val pointIsoTuple: Iso<Point2D, Tuple2<Int, Int>> = Iso(
+        get = { point -> point.x toT point.y },
+        reverseGet = { tuple -> Point2D(tuple.a, tuple.b) }
+    )
 
     /**
      * [Iso]는 유형 S와 A 사이의 동형사상, 즉 데이터 클래스와
@@ -21,8 +29,6 @@ class IsoSample {
      */
     @Test
     fun iso1() {
-        data class Point2D(val x: Int, val y: Int)
-
         val pointIsoTuple: Iso<Point2D, Tuple2<Int, Int>> = Iso(
             get = { point -> point.x toT point.y },
             reverseGet = { tuple -> Point2D(tuple.a, tuple.b) }
@@ -36,4 +42,41 @@ class IsoSample {
     }
 //    Tuple2(a=5, b=10)
 //    Point2D(x=5, y=10)
+
+    /**
+     * `Iso<Point2D, Tuple2<Int, Int>>`가 주어지면 우리는 `Iso<Tuple2<Int, Int>, Point2D>`도 가질 수 있습니다.
+     * 왜냐하면 `Iso`는 동등한 구조들 사이의 동형(Isomorphism)을 나타내기 때문에 우리는 그것을 뒤집을 수 있으니까요.
+     */
+    fun iso2() {
+        val reversedIso: Iso<Tuple2<Int, Int>, Point2D> = pointIsoTuple.reverse()
+    }
+
+    /**
+     * [Iso]를 사용하여 focus `A` 에서 작동하던 함수로 source `S`를 변경할 수 있습니다.
+     */
+    @Test
+    fun iso3() {
+        val point = Point2D(3, 5)
+        val addFive: (Tuple2<Int, Int>) -> Tuple2<Int, Int> = { tuple2 ->
+            (tuple2.a + 5) toT (tuple2.b + 5)
+        }
+        val modified = pointIsoTuple.modify(point, addFive)
+        println("modified = $modified")
+
+        /** 함수 `(A) -> A`를 함수 `(S) -> S`로 lift 할 수 있습니다.  */
+        val liftedAddFive: (Point2D) -> Point2D = pointIsoTuple.lift(addFive)
+        val modified2 = liftedAddFive(point)
+        println("modified2 = $modified2")
+    }
+//    modified = Point2D(x=8, y=10)
+//    modified2 = Point2D(x=8, y=10)n
+
+    /** Functor 매핑으로 동일한 작업을 수행할 수 있습니다. */
+    @Test
+    fun iso4() {
+        val point = Point2D(1, 2)
+        pointIsoTuple.modifyF(Try.functor(), point) { tuple ->
+            Try { tuple.a / 2 toT (tuple.b / 2) }
+        }
+    }
 }
